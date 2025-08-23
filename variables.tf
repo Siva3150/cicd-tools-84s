a3024bb7423ecdf589aa53927dd389ebde73f811
Siva prasad <sivaprasad3150@gmail.com>
2025-08-24 02:43:59 +0530

    user-profile

--- variables.tf
+++ variables.tf
@@ -7,18 +7,11 @@ variable "environment" {
 }
 
 variable "zone_id" {
-<<<<<<< HEAD
-    default = "Z02186033QPSEMV5IIWBW"
-}
-
-variable "zone_name" {
-    default = "sivadevops.fun"
-=======
     default = "Z07771633R956JE564RLG"
 }
 
 variable "zone_name" {
     default = "sivadevops.space"
->>>>>>> 08801e0 (user-profile)
+
 }
 
