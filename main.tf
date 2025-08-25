e125123e277e0b6458f078f5fdeef19fc7ee017d
Siva prasad <sivaprasad3150@gmail.com>
2025-08-25 12:26:35 +0530

    user-profile

--- main.tf
+++ main.tf
@@ -88,60 +88,60 @@ resource "aws_route53_record" "jenkins-agent" {
   allow_overwrite = true
 }
 
-#sonarqube
-resource "aws_instance" "sonarqube" {
-  ami           = local.ami_id
-  instance_type = "t3.large"
-  vpc_security_group_ids = [aws_security_group.sonarqube.id]
-  subnet_id = "subnet-02f5946970c07b2be" #replace your Subnet
+# #sonarqube
+# resource "aws_instance" "sonarqube" {
+#   ami           = local.ami_id
+#   instance_type = "t3.large"
+#   vpc_security_group_ids = [aws_security_group.sonarqube.id]
+#   subnet_id = "subnet-02f5946970c07b2be" #replace your Subnet
 
-  # need more for terraform
-  root_block_device {
-    volume_size = 20
-    volume_type = "gp3" # or "gp2", depending on your preference
-  }
-  user_data = file("sonarqube.sh")
-  tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}-sonarqube"
-    }
-  )
-}
+#   # need more for terraform
+#   root_block_device {
+#     volume_size = 20
+#     volume_type = "gp3" # or "gp2", depending on your preference
+#   }
+#   user_data = file("sonarqube.sh")
+#   tags = merge(
+#     local.common_tags,
+#     {
+#         Name = "${var.project}-${var.environment}-sonarqube"
+#     }
+#   )
+# }
 
-resource "aws_security_group" "sonarqube" {
-  name        =  "${var.project}-${var.environment}-sonarqube"
-  description = "Created for sonarqube"
+# resource "aws_security_group" "sonarqube" {
+#   name        =  "${var.project}-${var.environment}-sonarqube"
+#   description = "Created for sonarqube"
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}-sonarqube"
-    }
-  )
-}
+#   tags = merge(
+#     local.common_tags,
+#     {
+#         Name = "${var.project}-${var.environment}-sonarqube"
+#     }
+#   )
+# }
 
-resource "aws_route53_record" "sonarqube" {
-  zone_id = var.zone_id
-  name    = "sonarqube.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.sonarqube.public_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "sonarqube" {
+#   zone_id = var.zone_id
+#   name    = "sonarqube.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.sonarqube.public_ip]
+#   allow_overwrite = true
+# }
